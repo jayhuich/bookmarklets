de5cca244330485c78093c8bcc38439ab9188203
javier <29498758+javier-hui@users.noreply.github.com>
2021-08-06 13:53:28 +0800

    year

--- timetable.js
+++ timetable.js
@@ -1,5 +1,9 @@
 (function (d) {
     if (d.location.host == 'admlu65.ust.hk') {
+        var liTags = d.getElementsByTagName("li");
+        for (let element of liTags) {
+            if (element.hasAttribute("data-year")) element.setAttribute("data-year", (new Date()).getFullYear().slice(-2));
+        }
         var aTags = d.getElementsByTagName("a");
         for (let element of aTags) {
             if (element.textContent == ((new Date()).getMonth() > 5 ? "Fall" : "Spring")) {
